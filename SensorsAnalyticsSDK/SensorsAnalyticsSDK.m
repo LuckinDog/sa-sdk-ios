51a645bdbfc4bff70015d0d320de743f2f43d108
王灼洲 <wangzhuozhou@sensorsdata.cn>
2018-06-05 10:08:31 +0800

    fix bug
    Change-Id: I1a40d59aa350e2ed914b3714c3408a7360386b45

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
@@ -545,7 +545,9 @@ static SensorsAnalyticsSDK *sharedInstance = nil;
                 } else if (_debugMode == SensorsAnalyticsDebugAndTrack) {
                     alertMessage = @"现在您打开了'DEBUG_AND_TRACK'模式，此模式下会校验数据并且导入数据，数据出错时会以提示框的方式提示开发者，请上线前一定关闭。";
                 }
-                [self showDebugModeWarning:alertMessage withNoMoreButton:NO];
+                if (![self isLaunchedPassively]) {
+                    [self showDebugModeWarning:alertMessage withNoMoreButton:NO];
+                }
             }
 #endif
         }
