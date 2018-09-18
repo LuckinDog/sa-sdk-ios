c177290e5fa879c5d95b0f8b0c5bb58159e8810e
chuqsh <chuqsh1991@foxmail.com>
2018-09-18 17:51:29 +0800

    修改 float 精度丢失问题

--- SensorsAnalyticsSDK/JSONUtil.m
+++ SensorsAnalyticsSDK/JSONUtil.m
@@ -55,9 +55,13 @@
  */
 - (id)JSONSerializableObjectForObject:(id)obj {
     // valid json types
-    if ([obj isKindOfClass:[NSString class]] ||
-        [obj isKindOfClass:[NSNumber class]] ) {
+    if ([obj isKindOfClass:[NSString class]]) {
         return obj;
+    }
+    //防止 float 精度丢失
+    if ([obj isKindOfClass:[NSNumber class]]) {
+        NSDecimalNumber *number = [NSDecimalNumber decimalNumberWithDecimal:((NSNumber *)obj).decimalValue];
+        return number;
     }
     // recurse on containers
     if ([obj isKindOfClass:[NSArray class]]) {
