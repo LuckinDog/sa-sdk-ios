606975310120ae244314def463ab08562fbf4f0b
王灼洲 <wangzhuozhou@sensorsdata.cn>
2018-09-21 15:41:02 +0800

    修改版本号
    Change-Id: I0622f4233d33c53c652240c2597e5c8e2903657c

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
@@ -40,7 +40,7 @@
 #import "SACommonUtility.h"
 #import "SensorsAnalyticsSDK+Private.h"
 
-#define VERSION @"1.10.13"
+#define VERSION @"1.10.14"
 #define PROPERTY_LENGTH_LIMITATION 8191
 
 // 自动追踪相关事件及属性
