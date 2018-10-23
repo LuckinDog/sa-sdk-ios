0559919aca6b8260b6b0cf71daedd1d528454087
储强盛 <chuqiangsheng@sensorsdata.cn>
2018-10-23 20:32:58 +0800

    优化 NSObject+SensorsAnalyticsDelegate 逻辑

--- SensorsAnalyticsSDK/NSObject+SensorsAnalyticsDelegate.m
+++ SensorsAnalyticsSDK/NSObject+SensorsAnalyticsDelegate.m
@@ -13,11 +13,9 @@
 #import <objc/message.h>
 #import "AutoTrackUtils.h"
 #import "SensorsAnalyticsSDK.h"
-static NSMutableArray *arrChachedClassForCollectionViewDelegate = nil;
-static NSMutableArray *arrChachedClassForTableViewDelegate = nil;
 
 @interface NSObject (SensorsAnalyticsDelegate)
-+ (BOOL)addMethod:(Class)class sel:(SEL)sel method:(IMP)method;
+
 + (void)swapMethod:(Class)class origMethod:(SEL)origSelector newMethod:(SEL)newSelector;
 + (BOOL)hasMethod:(Class)class sel:(SEL)sel;
 @end
@@ -47,11 +45,10 @@ void sa_setDelegate(id obj ,SEL sel, id delegate){
             }
             if ([NSObject hasMethod:[delegate class] sel:@selector(tableView:didSelectRowAtIndexPath:)]){
                 SEL swizSel = NSSelectorFromString(@"swizzle_didSelectRowAtIndexPath");
-                if ([NSObject addMethod:[delegate class] sel:swizSel method:(IMP)swizzle_didSelectRowAtIndexPath]) {
-                    if (![arrChachedClassForTableViewDelegate containsObject:NSStringFromClass([delegate class])]) {
-                        [NSObject swapMethod:[delegate class] origMethod:swizSel newMethod:@selector(tableView:didSelectRowAtIndexPath:)];
-                        [arrChachedClassForTableViewDelegate addObject:NSStringFromClass([delegate class])];
-                    }
+                if (class_addMethod([delegate class], swizSel, (IMP)swizzle_didSelectRowAtIndexPath, "v@:@@")) {
+                    [NSObject swapMethod:[delegate class] origMethod:swizSel newMethod:@selector(tableView:didSelectRowAtIndexPath:)];
+                    
+                    
                 }
             }
         }else if ([obj isKindOfClass:UICollectionView.class]){
@@ -60,11 +57,8 @@ void sa_setDelegate(id obj ,SEL sel, id delegate){
             }
             if ([NSObject hasMethod:[delegate class] sel:@selector(collectionView:didSelectItemAtIndexPath:)]){
                 SEL swizSel = NSSelectorFromString(@"swizzle_didSelectItemAtIndexPath");
-                if ([NSObject addMethod:[delegate class] sel:swizSel method:(IMP)swizzle_didSelectItemAtIndexPath]) {
-                     if (![arrChachedClassForCollectionViewDelegate containsObject:NSStringFromClass([delegate class])]) {
-                         [NSObject swapMethod:[delegate class] origMethod:swizSel newMethod:@selector(collectionView:didSelectItemAtIndexPath:)];
-                         [arrChachedClassForCollectionViewDelegate addObject:NSStringFromClass([delegate class])];
-                     }
+                if (class_addMethod([delegate class], swizSel, (IMP)swizzle_didSelectItemAtIndexPath, "v@:@@")) {
+                    [NSObject swapMethod:[delegate class] origMethod:swizSel newMethod:@selector(collectionView:didSelectItemAtIndexPath:)];
                 }
             }
         }
@@ -77,10 +71,6 @@ void sa_setDelegate(id obj ,SEL sel, id delegate){
 }
 -(SADelegateProxy *)sensorsAnalyticsDelegateProxy{
     return objc_getAssociatedObject(self, @selector(setSensorsAnalyticsDelegateProxy:));
-}
-
-+ (BOOL)addMethod:(Class)class sel:(SEL)sel method:(IMP)method{
-    return class_addMethod(class, sel, method, "v@:@@");
 }
 
 + (void)swapMethod:(Class)class origMethod:(SEL)origSelector newMethod:(SEL)newSelector{
@@ -109,7 +99,6 @@ void sa_setDelegate(id obj ,SEL sel, id delegate){
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        arrChachedClassForTableViewDelegate = [[NSMutableArray alloc]init];
         SEL origSel_ = sel_getUid("setDelegate:");
         SEL swizzileSel = sel_getUid("sa_setDelegate:");
         Method origMethod = class_getInstanceMethod(self, origSel_);
@@ -129,7 +118,6 @@ void sa_setDelegate(id obj ,SEL sel, id delegate){
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        arrChachedClassForCollectionViewDelegate = [[NSMutableArray alloc]init];
         SEL origSel_ = sel_getUid("setDelegate:");
         SEL swizzileSel = sel_getUid("sa_setDelegate:");
         Method origMethod = class_getInstanceMethod(self, origSel_);
