a10404afd47ef4bd5fd8d4de49fcd845c63cbd97
储强盛 <chuqiangsheng@sensorsdata.cn>
2018-11-19 11:29:38 +0800

    增加非 ARC 环境漏设编译配置的报错

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
@@ -4,6 +4,11 @@
 //  Created by 曹犟 on 15/7/1.
 //  Copyright © 2015－2018 Sensors Data Inc. All rights reserved.
 
+#if ! __has_feature(objc_arc)
+#error This file must be compiled with ARC. Either turn on ARC for the project or use -fobjc-arc flag on this file.
+#endif
+
+
 #import <objc/runtime.h>
 #include <sys/sysctl.h>
 #include <stdlib.h>
