1c0c4b64a30c26264cadf99e3846657a2434c1ab
Zuoweixiang <xiangzuowei@sensorsdata.cn>
2018-12-29 15:41:42 +0800

    profileSet:pushId: 方法

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
@@ -3192,6 +3192,20 @@ static void sa_imp_setJSResponderBlockNativeResponder(id obj, SEL cmd, id reactT
     [[self people] set:profileDict];
 }
 
+- (void)profileSet:(NSString *)key pushId:(NSString *)pushId {
+    if ([key isKindOfClass:NSString.class] && key.length && [pushId isKindOfClass:NSString.class] && pushId.length) {
+        NSString * distinctId = self.getBestId;
+        NSString * keyOfPushId = [NSString stringWithFormat:@"%@_%@",distinctId,key];
+        NSString * valueOfPushId = [NSUserDefaults.standardUserDefaults valueForKey:keyOfPushId];
+        NSString * newValueOfPushId = [NSString stringWithFormat:@"%@_%@",distinctId,pushId];
+        if (![valueOfPushId isEqualToString:newValueOfPushId]) {
+            [self set:@{key:pushId}];
+            [NSUserDefaults.standardUserDefaults setValue:newValueOfPushId forKey:keyOfPushId];
+        }
+    }
+}
+
+
 - (void)setOnce:(NSDictionary *)profileDict {
     [[self people] setOnce:profileDict];
 }
