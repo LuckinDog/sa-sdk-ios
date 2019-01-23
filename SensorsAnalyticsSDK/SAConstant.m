5f10f332c93e54944b6e0d456ad71b3650e4e195
储强盛 <chuqiangsheng@sensorsdata.cn>
2019-01-23 16:21:01 +0800

    注释修改，NSUserDefaults 备注出来，防止后期冲突

--- SensorsAnalyticsSDK/SAConstant.m
+++ SensorsAnalyticsSDK/SAConstant.m
@@ -8,7 +8,7 @@
 
 #import "SAConstant.h"
 
-#pragma mark--event
+#pragma mark - event
 NSString * const SA_EVENT_TIME = @"time";
 NSString * const SA_EVENT_TRACK_ID = @"_track_id";
 NSString * const SA_EVENT_NAME = @"event";
@@ -20,7 +20,7 @@ NSString * const SA_EVENT_LIB = @"lib";
 NSString * const SA_EVENT_PROJECT = @"project";
 NSString * const SA_EVENT_TOKEN = @"token";
 
-#pragma mark--event name
+#pragma mark - event name
 // App 启动或激活
 NSString * const SA_EVENT_NAME_APP_START = @"$AppStart";
 // App 退出或进入后台
@@ -34,12 +34,12 @@ NSString * const SA_EVENT_NAME_APP_START_PASSIVELY = @"$AppStartPassively";
 
 NSString * const SA_EVENT_NAME_APP_SIGN_UP = @"$SignUp";
 
-#pragma mark--app install property
+#pragma mark - app install property
 NSString * const SA_EVENT_PROPERTY_APP_INSTALL_SOURCE = @"$ios_install_source";
 NSString * const SA_EVENT_PROPERTY_APP_INSTALL_DISABLE_CALLBACK = @"$ios_install_disable_callback";
 NSString * const SA_EVENT_PROPERTY_APP_INSTALL_USER_AGENT = @"$user_agent";
 NSString * const SA_EVENT_PROPERTY_APP_INSTALL_FIRST_VISIT_TIME = @"$first_visit_time";
-#pragma mark--autoTrack property
+#pragma mark - autoTrack property
 // App 首次启动
 NSString * const SA_EVENT_PROPERTY_APP_FIRST_START = @"$is_first_time";
 // App 是否从后台恢复
@@ -56,7 +56,7 @@ NSString * const SA_EVENT_PROPERTY_ELEMENT_SELECTOR = @"$element_selector";
 NSString * const SA_EVENT_PROPERTY_ELEMENT_CONTENT = @"$element_content";
 NSString * const SA_EVENT_PROPERTY_ELEMENT_TYPE = @"$element_type";
 
-#pragma mark--common property
+#pragma mark - common property
 NSString * const SA_EVENT_COMMON_PROPERTY_LIB = @"$lib";
 NSString * const SA_EVENT_COMMON_PROPERTY_LIB_VERSION = @"$lib_version";
 NSString * const SA_EVENT_COMMON_PROPERTY_LIB_DETAIL = @"$lib_detail";
@@ -86,7 +86,7 @@ NSString * const SA_EVENT_COMMON_OPTIONAL_PROPERTY_PROJECT = @"$project";
 NSString * const SA_EVENT_COMMON_OPTIONAL_PROPERTY_TOKEN = @"$token";
 
 
-#pragma mark--profile
+#pragma mark - profile
 NSString * const SA_PROFILE_SET = @"profile_set";
 NSString * const SA_PROFILE_SET_ONCE = @"profile_set_once";
 NSString * const SA_PROFILE_UNSET = @"profile_unset";
@@ -94,13 +94,8 @@ NSString * const SA_PROFILE_DELETE = @"profile_delete";
 NSString * const SA_PROFILE_APPEND = @"profile_append";
 NSString * const SA_PROFILE_INCREMENT = @"profile_increment";
 
-#pragma mark--others
+#pragma mark - NSUserDefaults
 NSString * const SA_SDK_TRACK_CONFIG = @"SASDKConfig";
 NSString * const SA_HAS_LAUNCHED_ONCE = @"HasLaunchedOnce";
 NSString * const SA_HAS_TRACK_INSTALLATION = @"HasTrackInstallation";
 NSString * const SA_HAS_TRACK_INSTALLATION_DISABLE_CALLBACK = @"HasTrackInstallationWithDisableCallback";
-
-
-
-
-
