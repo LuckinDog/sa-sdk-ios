14bcdcd7aecacca110a0ec3fca2f4f184e6a5d96
王灼洲 <wangzhuozhou@sensorsdata.cn>
2019-08-28 08:30:30 +0800

    修改版本号

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
@@ -65,7 +65,7 @@
 #import "SAAuxiliaryToolManager.h"
 #import "SAWeakPropertyContainer.h"
 
-#define VERSION @"1.11.11"
+#define VERSION @"1.11.12"
 
 static NSUInteger const SA_PROPERTY_LENGTH_LIMITATION = 8191;
 
