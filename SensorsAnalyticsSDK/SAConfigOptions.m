5b4ec495e59c04a69b05be486f43ae059d2654c1
温泉 <wenquan@sensorsdata.cn>
2020-04-07 10:47:44 +0800

    恢复现有逻辑

--- SensorsAnalyticsSDK/SAConfigOptions.m
+++ SensorsAnalyticsSDK/SAConfigOptions.m
@@ -81,7 +81,7 @@
     _flushBulkSize = flushBulkSize >= 50 ? flushBulkSize : 50;
 }
 
-- (void)setMaxCacheSize:(UInt64)maxCacheSize {
+- (void)setMaxCacheSize:(NSInteger)maxCacheSize {
     //防止设置的值太小导致事件丢失
     _maxCacheSize = maxCacheSize >= 10000 ? maxCacheSize : 10000;
 }
