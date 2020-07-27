b79cfba8ce19f132c531c91d4ca72f5f130b701b
温泉 <wenquan@sensorsdata.cn>
2020-07-27 11:31:27 +0800

    修改文件注释

--- SensorsAnalyticsSDK/RemoteConfig/SARemoteConfigModel.h
+++ SensorsAnalyticsSDK/RemoteConfig/SARemoteConfigModel.h
@@ -2,20 +2,20 @@
 //  SARemoteConfigModel.h
 //  SensorsAnalyticsSDK
 //
-//  Created by 向作为 on 2018/4/24.
-//  Copyright © 2015-2020 Sensors Data Co., Ltd. All rights reserved.
+// Created by wenquan on 2020/7/20.
+// Copyright © 2020 Sensors Data Co., Ltd. All rights reserved.
 //
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 #import <Foundation/Foundation.h>
