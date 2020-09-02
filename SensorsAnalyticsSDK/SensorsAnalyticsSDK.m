328325814f0b00964b74fd1a69953d2250c08bbb
王洋洋 <wangyangyang@sensorsdata.cn>
2020-09-02 15:13:12 +0800

    更改版本号为 2.1.6

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
@@ -82,7 +82,7 @@
 #import "SAVisualizedObjectSerializerManger.h"
 #import "SAEncryptSecretKeyHandler.h"
 
-#define VERSION @"2.1.5"
+#define VERSION @"2.1.6"
 
 static NSUInteger const SA_PROPERTY_LENGTH_LIMITATION = 8191;
 
