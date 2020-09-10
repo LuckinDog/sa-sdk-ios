960367a9400ecd50a6725ea1df570ca2e7b574a6
彭远洋 <pengyuanyang@sensorsdata.cn>
2020-09-10 17:02:20 +0800

    修改话术内容

--- SensorsAnalyticsSDK/Channel/SAChannelMatchManager.m
+++ SensorsAnalyticsSDK/Channel/SAChannelMatchManager.m
@@ -30,6 +30,7 @@
 #import "SAAlertController.h"
 #import "SAURLUtils.h"
 #import "SAReachability.h"
+#import "SALog.h"
 
 NSString *kChannelDebugFlagKey = @"sensorsdata_channel_debug_flag";
 
@@ -186,7 +187,7 @@ NSString *kChannelDebugFlagKey = @"sensorsdata_channel_debug_flag";
         [self showErrorMessage:@"数据接收地址错误，无法使用联调诊断工具"];
         return;
     }
-    NSString *project = [SAURLUtils queryItemsWithURLString:url.absoluteString][@"project"] ?: @"default";
+    NSString *project = [SAURLUtils queryItemsWithURLString:url.absoluteString][@"project_name"] ?: @"default";
     BOOL isEqualProject = [network.project isEqualToString:project];
     if (!isEqualProject) {
         [self showErrorMessage:@"App 集成的项目与电脑浏览器打开的项目不同，无法使用联调诊断工具"];
@@ -196,11 +197,11 @@ NSString *kChannelDebugFlagKey = @"sensorsdata_channel_debug_flag";
     SAReachability *reachability = [SAReachability reachabilityForInternetConnection];
     SANetworkStatus status = [reachability currentReachabilityStatus];
     if (status == SANotReachable) {
-        [self showErrorMessage:@"当前网络状况不可用，请检查网络状况后重试"];
+        [self showErrorMessage:@"当前网络不可用，请检查网络！"];
         return;
     }
 
-    NSString *title = @"即将开启「渠道管理白名单」模式";
+    NSString *title = @"即将开启联调模式";
     SAAlertController *alertController = [[SAAlertController alloc] initWithTitle:title message:@"" preferredStyle:SAAlertControllerStyleAlert];
     [alertController addActionWithTitle:@"确认" style:SAAlertActionStyleDefault handler:^(SAAlertAction * _Nonnull action) {
         if (![self isAppInstall] || ([self isNotEmptyIDFAOfAppInstall] && [SAIdentifier idfa])) {
@@ -247,8 +248,9 @@ NSString *kChannelDebugFlagKey = @"sensorsdata_channel_debug_flag";
             if (code == 1) {
                 [self showChannelDebugInstall];
             } else {
-                NSString *message = dict[@"message"] ?: @"添加白名单请求失败，请联系神策技术支持人员排查问题";
-                [self showErrorMessage:message];
+                NSString *message = dict[@"message"];
+                SALogError(@"%@", message);
+                [self showErrorMessage:@"添加白名单请求失败，请联系神策技术支持人员排查问题！"];
             }
         });
     }];
@@ -256,20 +258,22 @@ NSString *kChannelDebugFlagKey = @"sensorsdata_channel_debug_flag";
 }
 
 - (void)showChannelDebugInstall {
-    NSString *title = @"成功开启「渠道管理白名单」模式";
-    NSString *content = @"此模式下不需要卸载 App，点击下列 “激活” 按钮可以反复触发激活";
+    NSString *title = @"成功开启联调模式";
+    NSString *content = @"此模式下不需要卸载 App，点击“激活”按钮可反复触发激活。";
     SAAlertController *alertController = [[SAAlertController alloc] initWithTitle:title message:content preferredStyle:SAAlertControllerStyleAlert];
     [alertController addActionWithTitle:@"激活" style:SAAlertActionStyleDefault handler:^(SAAlertAction * _Nonnull action) {
         [self trackChannelDebugInstallEvent];
         [self showChannelDebugInstall];
     }];
+    [alertController addActionWithTitle:@"取消" style:SAAlertActionStyleCancel handler:nil];
     [alertController show];
 }
 
 - (void)showChannelDebugErrorMessage {
-    NSString *title = @"检测到 “设备码为空”，可能原因如下，请排查：\n";
-    NSString *content = @"1. 手机系统设置中选择禁用设备码；\n\n2. SDK 代码有误，请联系研发人员确认是否关闭“采集设备码”开关。\n\n 卸载并安装重新集成了修正的 SDK 的 App，再进行联调测试。";
+    NSString *title = @"检测到“设备码为空”，可能的原因如下，请排查：\n";
+    NSString *content = @"1. 手机系统设置中选择禁用设备码；\n\n2. 请联系研发人员确认是否关闭“采集设备码”开关。\n\n 排查后，请卸载并安装重新集成了 SDK 的 App，再进行联调。";
     SAAlertController *alertController = [[SAAlertController alloc] initWithTitle:title message:content preferredStyle:SAAlertControllerStyleAlert];
+    [alertController addActionWithTitle:@"确认" style:SAAlertActionStyleCancel handler:nil];
     [alertController show];
 }
 
