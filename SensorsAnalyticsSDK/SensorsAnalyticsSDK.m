1909d4ae5ff2173e977066b105d0d05523e79ded
yuqiang <yuqiang@sensorsdata.cn>
2020-09-29 10:49:46 +0800

    fix(用户关联): 删除 originalId
    $SignUp 事件中的 original_id 直接取值匿名 ID；同时删除 originalId 的相关代码
    
    https://doc.sensorsdata.cn/pages/viewpage.action?pageId=78956463

--- SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
+++ SensorsAnalyticsSDK/SensorsAnalyticsSDK.m
@@ -737,10 +737,6 @@ static SensorsAnalyticsSDK *sharedInstance = nil;
     return self.identifier.distinctId;
 }
 
-- (NSString *)originalId {
-    return self.identifier.originalId;
-}
-
 - (void)resetAnonymousId {
     dispatch_async(self.serialQueue, ^{
         [self.identifier resetAnonymousId];
@@ -1326,7 +1322,7 @@ static SensorsAnalyticsSDK *sharedInstance = nil;
                         eventName, SA_EVENT_NAME,
                         eventPropertiesDic, SA_EVENT_PROPERTIES,
                         bestId, SA_EVENT_DISTINCT_ID,
-                        self.originalId, @"original_id",
+                        self.anonymousId, @"original_id",
                         timeStamp, SA_EVENT_TIME,
                         type, SA_EVENT_TYPE,
                         libProperties, SA_EVENT_LIB,
@@ -2988,7 +2984,7 @@ static void sa_imp_setJSResponderBlockNativeResponder(id obj, SEL cmd, id reactT
             NSString *bestId = self.distinctId;
 
             if([type isEqualToString:@"track_signup"]) {
-                NSString *realOriginalId = self.originalId ?: self.anonymousId;
+                NSString *realOriginalId = self.anonymousId;
                 eventDict[@"original_id"] = realOriginalId;
             } else {
                 eventDict[SA_EVENT_DISTINCT_ID] = bestId;
