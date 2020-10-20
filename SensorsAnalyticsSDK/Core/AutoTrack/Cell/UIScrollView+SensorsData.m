96de6e35aec8dc0790fcd170d7976020b37820d4
yuqiang <yuqiang@sensorsdata.cn>
2020-10-20 18:14:03 +0800

    删除冗余函数

--- SensorsAnalyticsSDK/Core/AutoTrack/Cell/UIScrollView+SensorsData.m
+++ SensorsAnalyticsSDK/Core/AutoTrack/Cell/UIScrollView+SensorsData.m
@@ -26,51 +26,6 @@
 #import "SAConstants+Private.h"
 #import "SensorsAnalyticsSDK+Private.h"
 
-void sensorsdata_setDelegate(id obj, SEL sel, id delegate) {
-    // 获取 sensorsdata_setDelegate: 的方法名
-    SEL swizzileSel = sel_getUid("sensorsdata_setDelegate:");
-    // 调用 sensorsdata_setDelegate: 方法，由于之前已经交换，所以这里调用的是开发者自己实现的 setDelegate: 方法
-    ((void (*)(id, SEL, id))objc_msgSend)(obj, swizzileSel, delegate);
-    if (delegate == nil) {
-        return;
-    }
-    // 判断是否忽略 $AppClick 事件采集
-    if ([[SensorsAnalyticsSDK sharedInstance] isAutoTrackEventTypeIgnored:SensorsAnalyticsEventTypeAppClick]) {
-        return;
-    }
-
-    // 获取相应点击事件的方法（UITableView、UICollectionView）
-    SEL selector = NULL;
-    if ([obj isKindOfClass:[UITableView class]]) {
-        selector = @selector(tableView:didSelectRowAtIndexPath:);
-    } else if ([obj isKindOfClass:[UICollectionView class]]) {
-        selector = @selector(collectionView:didSelectItemAtIndexPath:);
-    }
-    // 使用委托类去 hook 点击事件方法
-    [SADelegateProxy proxyWithDelegate:delegate selector:selector];
-}
-
-void sensorsdata_swizzleSetDelegateMethod(Class _Nonnull cls) {
-    // 获取原始方法
-    SEL originalSel = sel_getUid("setDelegate:");
-    // 获取 swizzle 的方法
-    SEL swizzileSel = sel_getUid("sensorsdata_setDelegate:");
-    // 通过方法名获取原始实例方法的实现
-    Method originalMethod = class_getInstanceMethod(cls, originalSel);
-    // 获取原始方法的类型（参数及返回值的类型），也是 sensorsdata_setDelegate: 方法的类型
-    const char * type = method_getTypeEncoding(originalMethod);
-    // 在类中添加 sensorsdata_setDelegate: 方法
-    class_addMethod(cls, swizzileSel, (IMP)sensorsdata_setDelegate, type);
-    // 在添加之后，获取改方法
-    Method swizzleMethod = class_getInstanceMethod(cls, swizzileSel);
-    // 获取 swizzle 方法的实现，即 sensorsdata_setDelegate: 方法
-    IMP swizzleIMP = method_getImplementation(swizzleMethod);
-    // 将 sensorsdata_setDelegate: 方法的实现设置给 setDelegate: 方法
-    IMP originalIMP = method_setImplementation(originalMethod, swizzleIMP);
-    // 将之前 sensorsdata_setDelegate: 方法的实现设置给 setDelegate: 方法
-    method_setImplementation(swizzleMethod, originalIMP);
-}
-
 @implementation UITableView (SensorsData)
 
 #ifndef SENSORS_ANALYTICS_DISABLE_AUTOTRACK_UITABLEVIEW
