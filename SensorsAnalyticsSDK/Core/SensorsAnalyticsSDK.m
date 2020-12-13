88aa4cc75a02d5bfe8fba1c12d79ab5343c14f22
王洋洋 <wangyangyang@sensorsdata.cn>
2020-12-13 16:17:29 +0800

    修改版本号为 v2.2.3

--- SensorsAnalyticsSDK/Core/SensorsAnalyticsSDK.m
+++ SensorsAnalyticsSDK/Core/SensorsAnalyticsSDK.m
@@ -79,7 +79,7 @@
 #import "SAModuleManager.h"
 #import "SAChannelMatchManager.h"
 
-#define VERSION @"2.2.2"
+#define VERSION @"2.2.3"
 
 static NSUInteger const SA_PROPERTY_LENGTH_LIMITATION = 8191;
 
