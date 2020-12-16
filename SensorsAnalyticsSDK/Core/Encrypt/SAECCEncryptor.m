c12d980a1e7d813ae70e605ab2b4752f85707208
温泉 <wenquan@sensorsdata.cn>
2020-12-16 11:11:44 +0800

    将 ECC 处理密钥的逻辑移到初始化方法中

--- SensorsAnalyticsSDK/Core/Encrypt/SAECCEncryptor.m
+++ SensorsAnalyticsSDK/Core/Encrypt/SAECCEncryptor.m
@@ -27,23 +27,49 @@
 #import "SALog.h"
 #import "SAConstants+Private.h"
 
+@interface SAECCEncryptor ()
+
+/// 移除 EC: 前缀之后真正的公钥
+@property (nonatomic, copy) NSString *publicKey;
+
+@end
+
 @implementation SAECCEncryptor
+
+#pragma mark - Life Cycle
+
+- (instancetype)initWithSecretKey:(NSString *)secretKey {
+    self = [super initWithSecretKey:secretKey];
+    if (self) {
+        [self setupPublicKey];
+    }
+    return self;
+}
+
+- (void)setupPublicKey {
+    if (![SAValidator isValidString:self.secretKey]) {
+        SALogDebug(@"Enable ECC encryption but the secret key is invalid!");
+        return;
+    }
+
+    if (![self.secretKey hasPrefix:kSAEncryptECCPrefix]) {
+        SALogDebug(@"Enable ECC encryption but the secret key is not ECC key!");
+        return;
+    }
+
+    self.publicKey = [self.secretKey substringFromIndex:[kSAEncryptECCPrefix length]];
+}
 
 #pragma mark - Public Methods
 
 - (nullable NSString *)encryptObject:(NSData *)obj {
     if (![SAValidator isValidData:obj]) {
-        SALogDebug(@"Enable ECC encryption but the input obj is not NSString!");
+        SALogDebug(@"Enable ECC encryption but the input obj is invalid!");
         return nil;
     }
     
-    if (![SAValidator isValidString:self.secretKey]) {
-        SALogDebug(@"Enable ECC encryption but the public key is not NSString!");
-        return nil;
-    }
-    
-    if (![self.secretKey hasPrefix:kSAEncryptECCPrefix]) {
-        SALogDebug(@"Enable ECC encryption but the public key is not ECC key!");
+    if (![SAValidator isValidString:self.publicKey]) {
+        SALogDebug(@"Enable ECC encryption but the public key is invalid!");
         return nil;
     }
     
@@ -51,11 +77,10 @@
     SEL sel = NSSelectorFromString(@"encrypt:withPublicKey:");
     if ([crypto respondsToSelector:sel]) {
         NSString *string = [[NSString alloc] initWithData:(NSData *)obj encoding:NSUTF8StringEncoding];
-        NSString *publicKey = [self.secretKey substringFromIndex:[kSAEncryptECCPrefix length]];
-        
+
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-        NSString *result = [crypto performSelector:sel withObject:string withObject:publicKey];
+        NSString *result = [crypto performSelector:sel withObject:string withObject:self.publicKey];
 #pragma clang diagnostic pop
         
         return result;
