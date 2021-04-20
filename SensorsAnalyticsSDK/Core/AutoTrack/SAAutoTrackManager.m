f6c84d5215208c4ea733951c90e57e7d6d81b336
wenquan <wenquan@sensorsdata.cn>
2021-04-20 18:58:28 +0800

    去掉无用的代码

--- SensorsAnalyticsSDK/Core/AutoTrack/SAAutoTrackManager.m
+++ SensorsAnalyticsSDK/Core/AutoTrack/SAAutoTrackManager.m
@@ -104,12 +104,6 @@
     });
 }
 
-#pragma mark - Instance
-
-+ (SAAutoTrackManager *)sharedInstance {
-    return (SAAutoTrackManager *)[SAModuleManager.sharedInstance managerForModuleType:SAModuleTypeAutoTrack];
-}
-
 #pragma mark - Notification
 
 - (void)appLifecycleStateDidChange:(NSNotification *)sender {
