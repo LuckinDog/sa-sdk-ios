f64fd7fb4df9d545dae3302b726803883d3dffcc
yuqiang <yuqiang@sensorsdata.cn>
2021-04-23 16:28:58 +0800

    补充公共属性的单元测试

--- SensorsAnalyticsTests/SASuperPropertyTests.m
+++ SensorsAnalyticsTests/SASuperPropertyTests.m
@@ -54,6 +54,13 @@
     XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
 }
 
+- (void)testCurrentSuperProperties {
+    [self.superPorperty clearSuperProperties];
+    [self.superPorperty registerSuperProperties:@{@"abc123": @"abc123_value"}];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 1);
+}
+
+
 - (void)testRegisterSuperPropertiesForInvalid {
     [self.superPorperty clearSuperProperties];
     [self.superPorperty registerSuperProperties:@{@"123abc": @"123abcValue"}];
@@ -87,18 +94,170 @@
     XCTAssertTrue([(@{}) isEqualToDictionary:result2]);
 }
 
-
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testRegisterSuperPropertyForInvalidKey {
+    [self.superPorperty clearSuperProperties];
+    [self.superPorperty registerSuperProperties:@{@"123abc": @"123abcValue"}];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+- (void)testRegisterSuperPropertyForInvalidKey2 {
+    [self.superPorperty clearSuperProperties];
+    [self.superPorperty registerSuperProperties:@{@(123): @"123"}];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
+}
+
+- (void)testRegisterSuperPropertyForInvalidKey3 {
+    [self.superPorperty clearSuperProperties];
+
+    [self.superPorperty registerSuperProperties:@{@"date": @"date_value",
+                                                  @"normalKey": @"normalValue",
+                                                }];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
+
+    [self.superPorperty registerSuperProperties:@{@"datetime": @"datetimeValue",
+                                                  @"normalKey": @"normalValue",
+                                                }];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
+
+    [self.superPorperty registerSuperProperties:@{@"distinct_id": @"distinct_idValue",
+                                                  @"normalKey": @"normalValue",
+                                                }];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
+
+    [self.superPorperty registerSuperProperties:@{@"event": @"eventValue",
+                                                  @"normalKey": @"normalValue",
+                                                }];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
+
+    [self.superPorperty registerSuperProperties:@{@"events": @"eventsValue",
+                                                  @"normalKey": @"normalValue",
+                                                }];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
+
+    [self.superPorperty registerSuperProperties:@{@"first_id": @"first_idValue",
+                                                  @"normalKey": @"normalValue",
+                                                }];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
+
+    [self.superPorperty registerSuperProperties:@{@"id": @"idValue",
+                                                  @"normalKey": @"normalValue",
+                                                }];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
+
+    [self.superPorperty registerSuperProperties:@{@"original_id": @"original_idValue",
+                                                  @"normalKey": @"normalValue",
+                                                }];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
+
+    [self.superPorperty registerSuperProperties:@{@"properties": @"propertiesValue",
+                                                  @"normalKey": @"normalValue",
+                                                }];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
+
+    [self.superPorperty registerSuperProperties:@{@"second_id": @"second_idValue",
+                                                  @"normalKey": @"normalValue",
+                                                }];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
+
+    [self.superPorperty registerSuperProperties:@{@"time": @"timeValue",
+                                                  @"normalKey": @"normalValue",
+                                                }];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
+
+    [self.superPorperty registerSuperProperties:@{@"user_id": @"user_idValue",
+                                                  @"normalKey": @"normalValue",
+                                                }];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
+
+    [self.superPorperty registerSuperProperties:@{@"users": @"usersValue",
+                                                  @"normalKey": @"normalValue",
+                                                }];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
+
+}
+
+- (void)testRegisterSuperPropertyForInvalidKey4 {
+    [self.superPorperty clearSuperProperties];
+    [self.superPorperty registerSuperProperties:@{@"_aaaaaaaaaa_aaaaaaaaaa_aaaaaaaaaa_aaaaaaaaaa_aaaaaaaaaa_aaaaaaaaaa_aaaaaaaaaa_aaaaaaaaaa_aaaaaaaaaa_a": @"测试 key 长度"}];
+    XCTAssertTrue(self.superPorperty.currentSuperProperties.count == 0);
+}
+
+- (void)testUnregisterSameLetterSuperProperties {
+    [self.superPorperty clearSuperProperties];
+    [self.superPorperty registerSuperProperties:@{@"abc": @"abcValue",
+                                                  @"BCD": @"BCDValue",
+                                                  @"eFg": @"eFgValue",}];
+    [self.superPorperty unregisterSameLetterSuperProperties:@{@"ABC": @"ABCValue",
+                                                              @"EfG": @"EfGValue"}];
+    XCTAssertTrue([(@{@"BCD": @"BCDValue"}) isEqualToDictionary:self.superPorperty.currentSuperProperties]);
+}
+
+- (void)testUnregisterSameLetterSuperProperties2 {
+    [self.superPorperty clearSuperProperties];
+    [self.superPorperty registerSuperProperties:@{@"BCD": @"BCDValue"}];
+    [self.superPorperty unregisterSameLetterSuperProperties:@{@"bcd": @"bcdValue"}];
+    XCTAssertTrue([(@{}) isEqualToDictionary:self.superPorperty.currentSuperProperties]);
+}
+
+- (void)testUnregisterSameLetterSuperProperties3 {
+    [self.superPorperty clearSuperProperties];
+    [self.superPorperty registerSuperProperties:@{@"BCD": @"BCDValue"}];
+    [self.superPorperty unregisterSameLetterSuperProperties:@{@"abcd": @"bcdValue"}];
+    XCTAssertTrue([(@{@"BCD": @"BCDValue"}) isEqualToDictionary:self.superPorperty.currentSuperProperties]);
+}
+
+- (void)testRegisterDynamicSuperProperties {
+    [self.superPorperty registerDynamicSuperProperties:^NSDictionary<NSString *,id> * _Nonnull{
+        return @{@"hhh": @(123),
+                 @"jjj": @[@"j", @"jj", @"jjj"]};
     }];
+    NSDictionary *reuslt = [self.superPorperty acquireDynamicSuperProperties];
+    XCTAssertTrue([(@{@"hhh": @(123),@"jjj": @[@"j", @"jj", @"jjj"]}) isEqualToDictionary:reuslt]);
 }
+
+- (void)testRegisterDynamicSuperProperties2 {
+    [self.superPorperty registerDynamicSuperProperties:^NSDictionary<NSString *,id> * _Nonnull{
+        return @{@"hhh": @(123),
+                 @"123jjj": @[@"j", @"jj", @"jjj"]};
+    }];
+    XCTAssertNil([self.superPorperty acquireDynamicSuperProperties]);
+}
+
+- (void)testRegisterHybrid {
+    [self.superPorperty clearSuperProperties];
+    [self.superPorperty registerSuperProperties:@{@"fff": @(456)}];
+    [self.superPorperty registerDynamicSuperProperties:^NSDictionary<NSString *,id> * _Nonnull{
+        return @{@"hhh": @(123),
+                 @"jjj": @[@"j", @"jj", @"jjj"]};
+    }];
+    NSDictionary *reuslt = [self.superPorperty acquireDynamicSuperProperties];
+    XCTAssertTrue([(@{@"fff": @(456)}) isEqualToDictionary:self.superPorperty.currentSuperProperties]);
+    XCTAssertTrue([(@{@"hhh": @(123),@"jjj": @[@"j", @"jj", @"jjj"]}) isEqualToDictionary:reuslt]);
+}
+
+- (void)testRegisterHybrid2 {
+    [self.superPorperty clearSuperProperties];
+    [self.superPorperty registerSuperProperties:@{@"hhh": @(456)}];
+    [self.superPorperty registerDynamicSuperProperties:^NSDictionary<NSString *,id> * _Nonnull{
+        return @{@"hhh": @(123),
+                 @"jjj": @[@"j", @"jj", @"jjj"]};
+    }];
+    NSDictionary *reuslt = [self.superPorperty acquireDynamicSuperProperties];
+    XCTAssertTrue([(@{}) isEqualToDictionary:self.superPorperty.currentSuperProperties]);
+    XCTAssertTrue([(@{@"hhh": @(123),@"jjj": @[@"j", @"jj", @"jjj"]}) isEqualToDictionary:reuslt]);
+}
+
+- (void)testRegisterHybrid3 {
+    [self.superPorperty clearSuperProperties];
+    [self.superPorperty registerSuperProperties:@{@"HhH": @(456)}];
+    [self.superPorperty registerDynamicSuperProperties:^NSDictionary<NSString *,id> * _Nonnull{
+        return @{@"hhh": @(123),
+                 @"jjj": @[@"j", @"jj", @"jjj"]};
+    }];
+    NSDictionary *reuslt = [self.superPorperty acquireDynamicSuperProperties];
+    XCTAssertTrue([(@{}) isEqualToDictionary:self.superPorperty.currentSuperProperties]);
+    XCTAssertTrue([(@{@"hhh": @(123),@"jjj": @[@"j", @"jj", @"jjj"]}) isEqualToDictionary:reuslt]);
+}
+
 
 @end
