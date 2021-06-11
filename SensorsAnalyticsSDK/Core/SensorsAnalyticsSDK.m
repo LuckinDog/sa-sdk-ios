e75e65b63100a9af09c601f70e06baa44402fdf6
jg zhu <13913514919@189.cn>
2021-06-11 15:43:45 +0800

    Release 2.6.6

--- SensorsAnalyticsSDK/Core/SensorsAnalyticsSDK.m
+++ SensorsAnalyticsSDK/Core/SensorsAnalyticsSDK.m
@@ -73,7 +73,7 @@
 #import "SARemoteConfigEventObject.h"
 #import "SABaseEventObject+RemoteConfig.h"
 
-#define VERSION @"2.6.5"
+#define VERSION @"2.6.6"
 
 void *SensorsAnalyticsQueueTag = &SensorsAnalyticsQueueTag;
 
