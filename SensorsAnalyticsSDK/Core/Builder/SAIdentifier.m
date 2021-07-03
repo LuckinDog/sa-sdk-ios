de9fcea99ec9002dba3ecf9f2714f5c00ef77bf5
储强盛 <chuqiangsheng@sensorsdata.cn>
2021-07-03 15:32:55 +0800

    review 代码修改

--- SensorsAnalyticsSDK/Core/Builder/SAIdentifier.m
+++ SensorsAnalyticsSDK/Core/Builder/SAIdentifier.m
@@ -155,17 +155,17 @@
 /// mac SerialNumber（序列号）作为设备标识
 + (NSString *)uniqueHardwareMacSerialNumber {
     io_service_t platformExpert = IOServiceGetMatchingService(kIOMasterPortDefault,IOServiceMatching("IOPlatformExpertDevice"));
-    CFStringRef serialNumberAsCFString = NULL;
+    CFStringRef serialNumberRef = NULL;
     if (platformExpert) {
-        serialNumberAsCFString = IORegistryEntryCreateCFProperty(platformExpert,CFSTR(kIOPlatformSerialNumberKey),kCFAllocatorDefault, 0);
+        serialNumberRef = IORegistryEntryCreateCFProperty(platformExpert,CFSTR(kIOPlatformSerialNumberKey),kCFAllocatorDefault, 0);
         IOObjectRelease(platformExpert);
     }
-    NSString *serialNumberAsNSString = nil;
-    if (serialNumberAsCFString) {
-        serialNumberAsNSString = [NSString stringWithString:(__bridge NSString *)serialNumberAsCFString];
-        CFRelease(serialNumberAsCFString);
+    NSString *serialNumberString = nil;
+    if (serialNumberRef) {
+        serialNumberString = [NSString stringWithString:(__bridge NSString *)serialNumberRef];
+        CFRelease(serialNumberRef);
     }
-    return serialNumberAsNSString;
+    return serialNumberString;
 }
 #endif
 
