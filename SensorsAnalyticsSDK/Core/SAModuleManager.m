a17b0c16a04094908542205cba0c6465746968d0
yuqiang <yuqiang@sensorsdata.cn>
2021-09-15 11:37:35 +0800

    去除 modules 的 copy 操作

--- SensorsAnalyticsSDK/Core/SAModuleManager.m
+++ SensorsAnalyticsSDK/Core/SAModuleManager.m
@@ -261,10 +261,8 @@ static NSString * const kSAExceptionModuleName = @"Exception";
 
 - (NSDictionary *)properties {
     NSMutableDictionary *properties = [NSMutableDictionary dictionary];
-    // 这里需要做一次 copy 操作，避免多线程中同时操作 modules 导致崩溃
-    NSDictionary *dictionary = [self.modules copy];
     // 兼容使用宏定义的方式源码集成 SDK
-    [dictionary enumerateKeysAndObjectsUsingBlock:^(NSString *key, id<SAModuleProtocol> obj, BOOL *stop) {
+    [self.modules enumerateKeysAndObjectsUsingBlock:^(NSString *key, id<SAModuleProtocol> obj, BOOL *stop) {
         if (!([obj conformsToProtocol:@protocol(SAPropertyModuleProtocol)] && [obj respondsToSelector:@selector(properties)]) || !obj.isEnable) {
             return;
         }
@@ -419,9 +417,7 @@ static NSString * const kSAExceptionModuleName = @"Exception";
 
 - (NSString *)javaScriptSource {
     NSMutableString *source = [NSMutableString string];
-    // 这里需要做一次 copy 操作，避免多线程中同时操作 modules 导致崩溃
-    NSDictionary *dictionary = [self.modules copy];
-    [dictionary enumerateKeysAndObjectsUsingBlock:^(NSString *key, id<SAModuleProtocol> obj, BOOL *stop) {
+    [self.modules enumerateKeysAndObjectsUsingBlock:^(NSString *key, id<SAModuleProtocol> obj, BOOL *stop) {
         if (!([obj conformsToProtocol:@protocol(SAJavaScriptBridgeModuleProtocol)] && [obj respondsToSelector:@selector(javaScriptSource)]) || !obj.isEnable) {
             return;
         }
