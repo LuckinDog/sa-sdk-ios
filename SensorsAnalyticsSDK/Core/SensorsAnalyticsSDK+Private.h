6fceabe6ad69139ea1b13ba1d29361e33d0917a6
yuqiang <yuqiang@sensorsdata.cn>
2021-10-25 11:27:21 +0800

    代码调整

--- SensorsAnalyticsSDK/Core/SensorsAnalyticsSDK+Private.h
+++ SensorsAnalyticsSDK/Core/SensorsAnalyticsSDK+Private.h
@@ -60,7 +60,7 @@
 @property (nonatomic, strong, readonly) SAConfigOptions *configOptions;
 @property (nonatomic, strong, readonly) SANetwork *network;
 @property (nonatomic, strong, readonly) SASuperProperty *superProperty;
-@property (nonatomic, strong) SAEventTracker *eventTracker;
+@property (nonatomic, strong, readonly) SAEventTracker *eventTracker;
 @property (nonatomic, strong, readonly) dispatch_queue_t serialQueue;
 
 @end
